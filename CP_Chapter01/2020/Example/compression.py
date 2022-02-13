62b3c6eec04c869a5ce011ebd085ddf0e9008d5d
meticulousdev <meticulousdev@gmail.com>
2022-02-13 17:27:15 +0900

    bit string print update

--- CP_Chapter01/2020/Example/compression.py
+++ CP_Chapter01/2020/Example/compression.py
@@ -21,24 +21,6 @@ class CompressedGene:
             else:
                 raise ValueError("Invalid nucleotide:{}".format(nucleotide))
 
-    # and (&) operator test
-    # def _compress(self, gene: str) -> None:
-    #     self.bit_string: int = 1  
-    #     for nucleotide in gene.upper():
-    #         self.bit_string <<= 2
-    #         bintype_bit_string = bin(self.bit_string)
-    #         print(bintype_bit_string)
-    #         if nucleotide == "A":
-    #             self.bit_string &= 0b00
-    #         elif nucleotide == "C":
-    #             self.bit_string &= 0b01
-    #         elif nucleotide == "G":
-    #             self.bit_string &= 0b10
-    #         elif nucleotide == "T":
-    #             self.bit_string &= 0b11
-    #         else:
-    #             raise ValueError("Invalid nucleotide:{}".format(nucleotide))
-                
     def decompress(self) -> str:
         gene: str = ""
         for i in range(0, self.bit_string.bit_length() - 1, 2):
