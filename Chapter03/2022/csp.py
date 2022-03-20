167a8f946f68797cb999f92917cb84d4a438edad
meticulousdev <meticulousdev@gmail.com>
2022-03-21 03:01:48 +0900

    sudoku solver

--- Chapter03/2022/csp.py
+++ Chapter03/2022/csp.py
@@ -1,19 +1,21 @@
 from typing import Generic, TypeVar, Dict, List, Optional
 from abc import ABC, abstractmethod
 
+
 V = TypeVar('V')
 D = TypeVar('D')
 
 
 # class Constraint(Generic[V, D], metaclass=ABCMeta):
-class Constraint(Generic[V, D]):
-# class Constraint(Generic[V, D], ABC):
+# class Constraint(Generic[V, D]):
+class Constraint(Generic[V, D], ABC):
     def __init__(self, variables: List[V]) -> None:
         self.variables = variables
 
-    # @abstractmethod
+    @abstractmethod
     def satisfied(self, assignment: Dict[V, D]) -> bool:
             ...
+            # pass
         
 
 class CSP(Generic[V, D]):
