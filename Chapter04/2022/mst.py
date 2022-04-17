27db743644379d9a2420b8d521dd364359d1df82
meticulousdev <meticulousdev@gmail.com>
2022-04-17 21:33:16 +0900

    mst update

--- Chapter04/2022/mst.py
+++ Chapter04/2022/mst.py
@@ -10,6 +10,41 @@ WeightedPath = List[WeightedEdge]
 def total_weight(wp: WeightedPath) -> float:
     return sum([e.weight for e in wp])
 
+    
+# def visit(index: int, visited: List[bool], wg: WeightedGraph, pq: PriorityQueue) -> None:
+#     visited[index] = True
+#     for edge in wg.edges_for_index(index):
+#         if not visited[edge.v]:
+#             pq.push(edge)
+
+
+# def mst(wg: WeightedGraph[V], start: int = 0) -> Optional[WeightedPath]:
+#     if start > (wg.vertex_count - 1) or start < 0:
+#         return None
+
+#     result: WeightedPath = []
+#     pq: PriorityQueue[WeightedEdge] = PriorityQueue()
+#     visited: List[bool] = [False] * wg.vertex_count
+
+#     visit(start, visited, wg, pq)
+
+#     while not pq.empty:
+#         edge = pq.pop()
+#         if visited[edge.v]:
+#             continue
+
+#         result.append(edge)
+#         visit(edge.v, visited, wg, pq)
+
+#     return result
+
+
+# Jarnik's algorithm
+# 1. Pick an arbitrary vertex to include tin the minimum spanning tree.
+# 2. Find the lowest-weight edge connecting the minimum spanning tree 
+#    to the vertices not yet in the minimum spanning tree.
+# 3. Add the vertex at the end of that minimum edge to the minimum spanning tree.
+# 4. Repeat steps 2 and 3 until every vertex in the graph is in the minimum spanning tree.
 
 # mst : minimum spanning tree
 def mst(wg: WeightedGraph[V], start: int = 0) -> Optional[WeightedPath]:
@@ -78,7 +113,7 @@ if __name__ == "__main__":
     city_graph2.add_edge_by_vertices("New York", "Philadelphia", 81)
     city_graph2.add_edge_by_vertices("Philadelphia", "Washington", 123)
 
-    result: Optional[WeightedPath] = mst(city_graph2)
+    result: Optional[WeightedPath] = mst(city_graph2, 5)
     if result is None:
         print("No solution found!")
     else:
