69ab115b782b73f8d111bbabb73fce54b50ca2bb
meticulousdev <meticulousdev@gmail.com>
2022-05-10 00:50:50 +0900

    stack test

--- Chapter04/2022/Konigsberg.py
+++ Chapter04/2022/Konigsberg.py
@@ -1,29 +1,81 @@
+from tkinter.messagebox import NO
 from typing import TypeVar, List, Optional
 from named_graph import NamedGraph
 from named_edge import NamedEdge
 
-from generic_search import Queue
-
-from weighted_graph import WeightedGraph
-from weighted_edge import WeightedEdge
+from generic_search import Stack
 
 
 V = TypeVar('V')
 NamedPath = List[NamedEdge]
 
 
-def eulerian_trail(ne: NamedEdge[V], start: int = 0) -> Optional[NamedPath]:
-    if start > (ne.vertex_count - 1) or start < 0:
+def eulerian_trail(ng: NamedGraph[V], vi: List[str], start: int = 0) -> Optional[NamedPath]:
+    if start > (ng.vertex_count - 1) or start < 0:
         return None
     
     result: NamedPath = []
-    qe: Queue[NamedEdge] = Queue() 
-    visited: List[bool] = [False] * ne.vertex_count
-
+    sk: Stack[NamedEdge] = Stack() 
+    visited: List[bool] = [False] * ng.vertex_count
+    
     def visit(index: int):
+        # revision required
+        if vi[index] == "land":
+            for i, vertex_name in enumerate(vi):
+                if vertex_name == "land":
+                    visited[i] = False
+
         visited[index] = True
-        for edge in ne.edges_for_index(index):
+
+        for edge in ng.edges_for_index(index):
             if not visited[edge.v]:
-                ne.push(edge)
+                sk.push(edge)
 
     visit(start)
+    
+    while not sk.empty:
+        edge = sk.pop()
+        if visited[edge.v]:
+            continue
+
+        result.append(edge)
+        visit(edge.v)
+
+    return result
+
+
+def print_named_path(ng: NamedGraph, np: NamedPath) -> None:
+    for edge in np:
+        print(f"{ng.vertex_at(edge.u)} {edge.u_name} > {ng.vertex_at(edge.v)} {edge.v_name}")
+
+
+if __name__ == "__main__":
+    graph_Konigsberg: NamedGraph[str] = NamedGraph(["A", "B", "C", "D",
+                                                    "a", "b", "c", "d", "e", "f", "g"])
+    vertice_info: List[str] = ["land", "land", "land", "land",
+                               "bridge", "bridge", "bridge", "bridge", 
+                               "bridge", "bridge", "bridge"]
+
+    graph_Konigsberg.add_edge_by_vertices("A", "a", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("A", "b", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("A", "c", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("A", "d", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("A", "e", "land", "bridge")
+    
+    graph_Konigsberg.add_edge_by_vertices("B", "a", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("B", "b", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("B", "f", "land", "bridge")
+    
+    graph_Konigsberg.add_edge_by_vertices("C", "c", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("C", "d", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("C", "g", "land", "bridge")
+    
+    graph_Konigsberg.add_edge_by_vertices("D", "e", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("D", "f", "land", "bridge")
+    graph_Konigsberg.add_edge_by_vertices("D", "g", "land", "bridge")
+
+    result: Optional[NamedPath] = eulerian_trail(graph_Konigsberg, vertice_info, 0)
+    if result is None:
+        print("No solution found!")
+    else:
+        print_named_path(graph_Konigsberg, result)
