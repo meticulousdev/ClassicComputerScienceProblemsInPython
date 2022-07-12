7952a8fd195e96e7e0c09f6864defb6b8495d2af
meticulousdev <meticulousdev@gmail.com>
2022-07-13 00:31:25 +0900

    comments update

--- Chapter05/2022/Test/test_type_isinstance.py
+++ Chapter05/2022/Test/test_type_isinstance.py
@@ -23,8 +23,6 @@ class Desktop(Computer):
 class Laptop(Computer):
     def __init__(self, name: str, cpu_name: str, gpu_name: str, battery_name: str):
         super().__init__(name, cpu_name, gpu_name)
-        self.cpu_name = cpu_name
-        self.gpu_name = gpu_name
         self.battery_name = battery_name
 
     def get_laptop_info(self):
@@ -36,24 +34,29 @@ if __name__ == "__main__":
     my_computer: Laptop = Laptop('AMD Laptop', 'AMD Rygen 5', 'Radeon', 'ABC Battery')
     your_computer: Laptop = Laptop('Intel Laptop', 'Intel i5', 'RTX 3060', 'DEF Battery')
 
+    # code
     print(f"type(my_computer)  : {type(my_computer)}")
     print(f"type(your_computer): {type(your_computer)}")
     print()
 
+    # code
     print(f"type(my_computer) == Computer: {type(my_computer) == Computer}")
     print(f"type(my_computer) == Desktop : {type(my_computer) == Desktop}")
     print(f"type(my_computer) == Laptop  : {type(my_computer) == Laptop}")
     print()
 
+    # code
     print(f"isinstance(my_computer, Computer): {isinstance(my_computer, Computer)}")
     print(f"isinstance(my_computer, Desktop) : {isinstance(my_computer, Desktop)}")
     print(f"isinstance(my_computer, Laptop)  : {isinstance(my_computer, Laptop)}")
     print()
 
+    # code
     print(f"type(my_computer).get_laptop_info: {type(my_computer).get_laptop_info}")
     print(f"my_computer.get_laptop_info      : {my_computer.get_laptop_info}")
     print()
 
+    # code
     print("type(my_computer).get_laptop_info(my_computer):")
     type(my_computer).get_laptop_info(my_computer)
     print()
