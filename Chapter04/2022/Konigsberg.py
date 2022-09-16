3293e1f14c3e4910271fc6de5a420c1cecefbfd8
meticulousdev <meticulousdev@gmail.com>
2022-09-16 14:43:40 +0900

    Konigsberg Bridge - CSP

---
+++ Chapter04/2022/Konigsberg.py
@@ -0,0 +1,46 @@
+from tkinter.messagebox import NO
+from typing import TypeVar, List, Dict, Optional
+from graph import Graph
+from csp import Constraint, CSP
+
+
+V = TypeVar('V')
+
+
+# TODO csp ?
+class Konigsberg(Constraint[str, str]):
+    def __init__(self, lands: str, bridges: str) -> None:
+        super().__init__([lands, bridges])
+        self.lands: str = lands
+        self.bridges: str = bridges
+
+    def satisfied(self, assignment: Dict[str, str]) -> bool:
+        test: Dict[str, str]
+        return test
+
+
+if __name__ == "__main__":
+    lands: List[str] = ["A", "B", "C", "D"]
+    bridges: List[str] = ["a", "b", "c", "d", "e", "f", "g"]
+    graph_total: List[str] = lands + bridges
+    graph_Konigsberg: Graph[str] = Graph(graph_total)
+
+    graph_Konigsberg.add_edge_by_vertices("A", "a")
+    graph_Konigsberg.add_edge_by_vertices("A", "b")
+    graph_Konigsberg.add_edge_by_vertices("A", "c")
+    graph_Konigsberg.add_edge_by_vertices("A", "d")
+    graph_Konigsberg.add_edge_by_vertices("A", "e")
+    
+    graph_Konigsberg.add_edge_by_vertices("B", "a")
+    graph_Konigsberg.add_edge_by_vertices("B", "b")
+    graph_Konigsberg.add_edge_by_vertices("B", "f")
+    
+    graph_Konigsberg.add_edge_by_vertices("C", "c")
+    graph_Konigsberg.add_edge_by_vertices("C", "d")
+    graph_Konigsberg.add_edge_by_vertices("C", "g")
+    
+    graph_Konigsberg.add_edge_by_vertices("D", "e")
+    graph_Konigsberg.add_edge_by_vertices("D", "f")
+    graph_Konigsberg.add_edge_by_vertices("D", "g")
+    
+    print(graph_Konigsberg)
