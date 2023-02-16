8ad2a35685cebf5164cf1ff608eba0774eb9ab21
meticulousdev <meticulousdev@gmail.com>
2023-02-16 17:53:25 +0900

    TestCode

--- Chapter01/2022/Test/test_list_List.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-
-
-Test_list: list[float]
-Test_List: List[float]
-
-print(f"type(list): {type(list)}")
-print(f"type(int) : {type(int)}")
-print(f"type(List): {type(List)}")
